decfbb2379f1aa0d9844db5c8da1ce304894b35b
Razvan Cristian Lung <lung.razvan@gmail.com>
2018-12-10 15:21:32 +0200

    update the export list

--- lib/algoliasearch.dart
+++ lib/algoliasearch.dart
@@ -1,1 +1,7 @@
 library algoliasearch;
+
+export 'src/client.dart';
+export 'src/places/places_client.dart';
+export 'src/places/places_query.dart';
+export 'src/query.dart';
+export 'src/request_options.dart';
--- lib/src/helpers/browser_iterator.dart
+++
@@ -1,3 +0,0 @@
-// File created by
-// Lung Razvan <long1eu>
-// on 2018-12-05
